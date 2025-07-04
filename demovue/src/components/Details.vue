4547f71f1f8403148d3c00c0ade0f505bb0a3344
khun ly <khun.ly@bstorm.be>
2025-07-04 14:00:43 +0200

    removevue

--- demovue/src/components/Details.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-const {model} = defineProps(['model']);
-</script>
-<template>
-<h1>{{model.name}}</h1>
-<img :src="model.sprites.front_default" />
-<p>Poids: {{model.weight / 10}} kg</p>
-<p>Taille: {{model.height * 10}} cm</p>
-</template>
